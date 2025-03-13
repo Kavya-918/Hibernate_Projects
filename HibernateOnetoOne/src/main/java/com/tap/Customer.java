c88b14bdc2b462781661b5ba3a4a68def9752ea9
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-03-13 10:24:26 -0700

    Add files via upload

---
+++ HibernateOnetoOne/src/main/java/com/tap/Customer.java
@@ -0,0 +1,53 @@
+package com.tap;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+
+public class Customer {
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="cust_details_id")
+	private CustomerDetails customerDetails;
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setCustomerDetails(CustomerDetails customerDetails) {
+		this.customerDetails = customerDetails;
+	}
+	public CustomerDetails getCustomerDetails() {
+		return customerDetails;
+	}
+	
+}
